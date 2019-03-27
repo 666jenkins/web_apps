f1bd3775ddbb4e3f0a4771e90bd759c7c00408f2
BIT Student <classroom@bgit.rs>
2019-03-27 18:14:13 +0100

    konf/react

--- konfigurator/script/data.js
+++ konfigurator/script/data.js
@@ -1,14 +1,31 @@
 class Product {
-    constructor (input) {
-        this.title = input.slice(input.indexOf('Srbija. ') + 8, input.indexOf('karakteristike'))
-        this.price = input.slice(input.indexOf('cena od <b>') + 11, input.indexOf('</b> do'))
+    constructor(input) {
+        this.getPrice = () => {
+
+            let index1;
+            let index2;
+
+            if (input.includes('cena od <b>')) {
+                index1 = input.indexOf('cena od <b>') + 11;
+                index2 = input.indexOf('</b> do');
+            } else {
+                index1 = input.indexOf('cena <b>') + 8;
+                index2 = input.indexOf('</b> din')
+            }
+
+            return input.slice(index1, index2)
+        }
+
+        this.title = input.slice(input.indexOf('<h1>') + 4, input.indexOf('</h1>') - 5)
+        this.price = this.getPrice();
         this.priceInt = Number(this.price.slice(0, this.price.indexOf('.')) + this.price.slice(this.price.indexOf('.') + 1, this.price.length - 3))
+
     }
 }
 
 const fetchPage = () => {
 
-    const url = "https://www.eponuda.com/racunari-procesori-cene/amd-ryzen-5-2600-6-core-3-4-ghz-socket-am4-procesor-cena-434702"
+    const url = "https://www.eponuda.com/4k-ultra-hd-monitori/samsung-lu28d590ds-4k-uhd-cena-333821"
 
     const fetchItem = fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
         .then(response => {
@@ -16,6 +33,7 @@ const fetchPage = () => {
         })
         .then(data => {
 
+            console.log(data.contents);
             const item = new Product(data.contents)
 
             console.log(item);
--- konfigurator/script/ui.js
+++ konfigurator/script/ui.js
@@ -1,14 +1,14 @@
 const printItem = item => {
 
     $('.list-group')
-    .append(
-        $(`<li>${item.title}</li>`)
-        .addClass("list-group-item d-flex justify-content-between align-items-center")
         .append(
-            $(`<span>${item.price} din</span>`)
-            .addClass("badge badge-primary badge-pill")
+            $(`<li>${item.title}</li>`)
+                .addClass("list-group-item d-flex justify-content-between align-items-center")
+                .append(
+                    $(`<span>${item.price} din</span>`)
+                        .addClass("badge badge-primary badge-pill")
+                )
         )
-    )
 }
 
 export {
